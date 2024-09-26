2d297e34ae6c9ec6db3fdc7f571c2b5555653e90
Francesco Malandrino <fra.malandrino@gmail.com>
2024-09-26 21:28:41 +0200

    chore: Env file with API const and angular.json fix for use environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const ENV = {
+    production: true,
+    api: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/',
+    key: 'cf182eb482msh0cf768cef2fa79ap1e6143jsn07159361db23',
+    host: 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+}
