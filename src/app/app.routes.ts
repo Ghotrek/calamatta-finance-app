c27ca6021a6f0f28407ddceefd472648ca2af9e3
Francesco Malandrino <fra.malandrino@gmail.com>
2024-09-26 21:11:10 +0200

    Firt Commit Skeleton and project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        title: 'Calamatta Stock App',
+        loadComponent: () => import('./home/home.component').then(c => c.HomeComponent),
+    },
+    {
+        path: '**',
+        title: 'Page not found',
+        loadComponent: () => import('./not-found/not-found.component').then(c => c.NotFoundComponent),
+    }
+];
