4a7d14eb44feb15c372276067dd7718ba3f4281a
Francesco Malandrino <fra.malandrino@gmail.com>
2024-09-27 09:47:03 +0200

    feat: Stock Card, toggle for enable and disable update

--- src/app/home/stock/stock.component.ts
+++ src/app/home/stock/stock.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, ElementRef, input, signal, Signal, ViewChild } from '@angular/core';
+import { QuoteResult } from '../../model/stock.interface';
 
 @Component({
   selector: 'app-stock',
@@ -9,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class StockComponent {
 
+  inputStock = input.required<QuoteResult>();
+  stockRefresh = true;
+  stockSignal: Signal<QuoteResult> = this.inputStock;
+  storedStocks = signal<QuoteResult | null>(null)
+  stock = computed(() => {
+    if (this.stockRefresh) {
+      console.log(`Stock ${this.stockSignal().symbol} it's refreshed`);
+      return this.stockSignal();
+    } else {
+      console.log(`Stock ${this.stockSignal().symbol} not refreshed`);
+      return this.storedStocks();
+    }
+  });
+  @ViewChild('stockCard') stockCard!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.storedStocks.set(this.stockSignal());
+    this.setStatus(this.stockRefresh);
+  }
+
+  setStatus(checked: boolean) {
+    this.stockRefresh = checked;
+    if (checked) {
+      this.stockCard.nativeElement.classList.remove('bg-card-disabled');
+      this.setStockCardBackGround(this.stock()!.regularMarketChange);
+    } else {
+      this.stockCard.nativeElement.classList.remove('bg-card-red');
+      this.stockCard.nativeElement.classList.remove('bg-card-green');
+      this.stockCard.nativeElement.classList.add('bg-card-disabled');
+    }
+  }
+
+  setStockCardBackGround(regularMarketChange: number) {
+    if (regularMarketChange >= 0) {
+      this.stockCard.nativeElement.classList.add('bg-card-green');
+    } else {
+      this.stockCard.nativeElement.classList.add('bg-card-red');
+    }
+  }
 }
