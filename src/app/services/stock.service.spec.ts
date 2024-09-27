292d190afd01a42b2766dfa9b72cdb4c199ee1de
Francesco Malandrino <fra.malandrino@gmail.com>
2024-09-27 14:37:57 +0200

    test: Mock file and stock.serveice test

--- src/app/services/stock.service.spec.ts
+++ src/app/services/stock.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StockService } from './stock.service';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { firstValueFrom } from 'rxjs';
+import { MARKET_URL } from '../common/constants/url.constants';
+import { ENV } from '../../environments/environment';
+import { MockMarketResponse } from '../../market-mock-for-test';
 
 describe('StockService', () => {
   let service: StockService;
+  let httpClient: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        StockService,
+        provideExperimentalZonelessChangeDetection(),
+        provideHttpClient(),
+        provideHttpClientTesting(),],
+    });
     service = TestBed.inject(StockService);
+    httpClient = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be get data from http', async () => {
+    const marketStocks$ = service.getStock();
+    const marketStocksPromise = firstValueFrom(marketStocks$);
+    const req = httpClient.expectOne(`${ENV.api}${MARKET_URL}?region=US&symbols=AMD,IBM,AAPL,NVDA`, 'Request to load the marcket stocks');
+    expect(req.request.method).toBe('GET');
+    req.flush(MockMarketResponse);
+    expect(await marketStocksPromise).toEqual(MockMarketResponse.quoteResponse.result);
+    httpClient.verify();
   });
 });
