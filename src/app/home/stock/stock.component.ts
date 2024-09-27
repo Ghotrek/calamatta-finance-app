7d687163b8d681c2a2c29617ae84c9c511968d4d
Francesco Malandrino <fra.malandrino@gmail.com>
2024-09-27 12:52:15 +0200

    chore: add code doc and comment

--- src/app/home/stock/stock.component.ts
+++ src/app/home/stock/stock.component.ts
@@ -9,11 +9,18 @@ import { QuoteResult } from '../../model/stock.interface';
   styleUrl: './stock.component.scss'
 })
 export class StockComponent {
-
   inputStock = input.required<QuoteResult>();
+  
+  //Used for update Stock UI, if true UI render updated data in false UI render stored data
   stockRefresh = true;
+
+  //this signal it's required for computed signal
   stockSignal: Signal<QuoteResult> = this.inputStock;
+  
+  //This property it's init after View Init and used for mantain old data in case user turn off the update
   storedStock = signal<QuoteResult | null>(null)
+  
+  //Computed signal evaluate stockRefresh and it's true new data is returned, if false returnded data stored into storedStock property
   stock = computed(() => {
     if (this.stockRefresh) {
       console.log(`Stock ${this.stockSignal().symbol} it's refreshed`);
@@ -23,6 +30,7 @@ export class StockComponent {
       return this.storedStock();
     }
   });
+
   @ViewChild('stockCard') stockCard!: ElementRef;
 
   ngAfterViewInit(): void {
@@ -30,6 +38,13 @@ export class StockComponent {
     this.setStatus(this.stockRefresh);
   }
 
+  /**
+   * @method
+   * @description This method update is used from toggle button and update UI. 
+   *              When checked param is true disable class it's removed and called setStockCardBackGround method,
+   *              else remove class that show if strock it's in positive or not and add disabled class.
+   * @param {boolean} checked - Refer to region where API get market quotas
+   */
   setStatus(checked: boolean) {
     this.stockRefresh = checked;
     if (checked) {
@@ -42,6 +57,12 @@ export class StockComponent {
     }
   }
 
+  /**
+   * @method
+   * @description When param regularMarketChange is > or = to 0 added class that indicate stock gain in positive (green).
+   *              If param is negative added class that indicate stock it negative (red).
+   * @param {number} regularMarketChange - Refer to region where API get market quotas
+   */
   setStockCardBackGround(regularMarketChange: number) {
     if (regularMarketChange >= 0) {
       this.stockCard.nativeElement.classList.add('bg-card-green');
